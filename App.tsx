75c504947e51ba452dbae52efd9371abbbd6b153
Kotaro-Kitamura <kotarokitamura1983@gmail.com>
2020-12-23 13:10:04 +0900

    modified App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet,
+  Text, 
+  View,
+  Pressable,
+  Image,
+  SafeAreaView,
+  Dimensions,
+  Platform,
+ } from 'react-native';
 
 export default function App() {
   return (
